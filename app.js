e00d65ff36c6f00926d9751afce5260047278c61
Vanessa Lane <vlane@23andme.com>
2020-07-16 21:10:05 -0700

    add .gitignore

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const generatePage = require("./src/page-template.js");
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, githubName] = profileDataArgs;
 
-
 fs.writeFile("index.html", generatePage(name, githubName), err => {
     if (err) throw err;
     console.log('Portfolio complete! Check out index.html to see the output!');
